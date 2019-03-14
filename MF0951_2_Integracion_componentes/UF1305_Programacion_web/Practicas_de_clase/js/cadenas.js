9b1b526ac8034f419e400b39d42417f55e0f6324
nita86 <ana.rodriguezalv@gmail.com>
2019-03-14 13:57:58 +0100

    14/03

--- MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_de_clase/js/cadenas.js
+++ MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_de_clase/js/cadenas.js
@@ -28,10 +28,9 @@ function caractercode() {
 //Método fromCharCode()
 function personaje() {
     var Frase =prompt("introduce una serie de números del 32 al 136");
-    var convertir =parseInt(Frase);
-    var tipo = typeof(convertir);
-    alert (tipo);
-    
+    var convertir =parseInt(Frase);//convierte el string del prompt en number
+    var resultado =String.fromCharCode(convertir);
+    alert("el caracter Unicode correspondiente al número que has introducido es: "+resultado);
 }
 
 //Método toUpperCase()
@@ -91,7 +90,55 @@ function aparicionUltima() {
 
 //Método localeCompare()
 function locale() {
+    var Frase1 =prompt("introduce una frase");
+    var Frase2 =prompt("introduce otra frase");
+    var resultado =Frase1.localeCompare(Frase2);
+    alert("el orden alfabético de las palabras que has introducido es: "+resultado);
 }
 
 //Método match()
-function
+function encuentra() {
+    var Frase =prompt("introduce una frase");
+    var letra =prompt("introduce una letra o patrón");
+    var buscar =Frase.match(letra);
+    alert(buscar);
+}
+
+//Método repeat()
+function repetir() {
+    var Frase =prompt("introduce una frase");
+    var numero =prompt("introduce un numero");
+    var valor =parseInt(numero);
+    var resultado= Frase.repeat(valor);
+    alert("ahora tu frase se repite tantas veces como hayas decidido"+resultado);
+}
+
+//Método replace()
+function remplazar() {
+    var Frase =prompt("introduce una frase");
+    var palabra1 =prompt("introduce la palabra que quieres quitar");
+    var palabra2 =prompt("introduce la otra palabra");
+    var resultado =Frase.replace(palabra1, palabra2);
+    alert("tras el cambio, tu frase queda así: "+resultado);
+}
+
+//Método search()
+function busca() {
+    var Frase=prompt("introduce una frase");
+    var palabra1 =prompt("introduce la palabra que quieres buscar");
+    var resultado= Frase.search(palabra1);
+    alert("en tu frase, la palabra que has buscado está en la posición: "+resultado);
+}
+
+//Método slice()
+function cortar() {
+    var Frase=prompt("tu email");
+    var arroba =Frase.search(/@/);//busca @
+    var punto = Frase.search(/\./);//busca .
+    var usuario = Frase.slice(0,arroba);
+    var dominio= Frase.slice(arroba+1, punto);
+    var extension =Frase.slice(punto+1);
+    alert("tu email es: "+"/n"+usuario+"/n"+dominio+"/n"+extension);
+    
+
+}
