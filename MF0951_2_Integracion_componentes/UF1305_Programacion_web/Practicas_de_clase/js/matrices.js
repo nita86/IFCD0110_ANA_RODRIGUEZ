21a8058f6130060a00320cea0e1842ac921a80e1
nita86 <ana.rodriguezalv@gmail.com>
2019-03-21 09:44:08 +0100

    21/03_newArray_Roland

--- MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_de_clase/js/matrices.js
+++ MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_de_clase/js/matrices.js
@@ -2,16 +2,15 @@
 
 //newArray()
 function nuevoArray() {
-    var color1= prompt("introduce un color");
+    /* var color1= prompt("introduce un color");
     var color2= prompt("introduce un color");
     var color3= prompt("introduce un color");
-    var colores =
-        ("\"" +color1+ "\","+
-         "\""+color2+"\","+
-         "\""+color3+ "\"");
-    alert(colores);
+    alert(colores); */ 
+    var colores;
     var array =new Array(colores);
+    for (var i=0;i<3;i++) {
+        array [i] =prompt("introduce un color");
+    }
     var tipo= typeof(array);
-    alert(array +"\n"+"de tipo typeof "+tipo+"\n"+"de largo "+array.length);
-
+    alert(array +"\n de tipo typeof= "+tipo+"\n de largo "+array.length);
 }
