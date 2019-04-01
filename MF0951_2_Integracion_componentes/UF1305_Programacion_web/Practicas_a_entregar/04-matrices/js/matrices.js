521f0daebd14e2fc5d340ecb6b82c68cb60bc06e
nita86 <ana.rodriguezalv@gamil.com>
2019-04-02 00:05:13 +0200

    matrices 1-3 terminado

--- MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_a_entregar/04-matrices/js/matrices.js
+++ MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_a_entregar/04-matrices/js/matrices.js
@@ -2,18 +2,36 @@
 
 //ejercicio1
 function ejercicio1() {
-
+    var entrada=document.getElementById("text1").value;//obtiene string
+    var comas=entrada.replace(/,/g, " ");//quitar comas y poner espacios en su lugar
+    var array=comas.split(" ");//quita espacios y convierte en un array
+    array.splice (0,0, "Grupo Volkswagen:");//introducir los valores al inicio
+    document.getElementById('etiqueta1').innerHTML=array;
 }
 
-//ejercico2
-function ejercicio2() { 
-    var matriz=new Array("Seat", "Audi", "Skoda");
-    var textoIncluir=document.getElementById("text2").value;
-    var cortar=textoIncluir.split(" ");//separamos cada palabra y las transformamos en valores individuales
-    var primerEspacio=inicial.indexOf(" ");
-    var primerValor=inicial.slice(0,primerEspacio);
-    var ultimoEspacio=inicial.lastIndexOf(" ");
-    var segundoValor=inicial.slice(primerEspacio+1, ultimoEspacio);
-    var tercerValor=inicial.slice(ultimoEspacio+1);
-    var incluir=matriz.push("")
+//ejercicio 2
+function ejercicio2() {
+    var entrada=document.getElementById("text2").value;//obtiene string
+    var comas=entrada.replace(/,/g, " ");//quitar comas y poner espacios en su lugar
+    var array=comas.split(" ");//quita espacios y convierte en un array
+    array.splice(0,0, "Grupo Volkswagen:");//introducir los valores al inicio
+    array.push("Volkswagen", "Bentley", "Bugatti");//introducir los valores al final
+    document.getElementById('etiqueta2').innerHTML=array;
 }
+
+//ejercicio 3
+function ejercicio3() {
+    var entrada=document.getElementById("text3").value;//obtiene string
+    var comas=entrada.replace(/,/g, " ");//quitar comas y poner espacios en su lugar
+    var array=comas.split(" ");//quita espacios y convierte en un array
+    array.splice(0,0, "Grupo Volkswagen:");//introducir los valores al inicio
+    array.push("Volkswagen", "Bentley", "Bugatti");//introducir los valores al final
+    array.splice(1,0,"Ducati", "Lamborghini", "Porsche");//introducir los valores al en medio
+    var cadena=array.toString();
+    var posicionComa=cadena.indexOf(",");
+    var quitaComa1=cadena.slice(0,17);
+    var final = quitaComa1 + cadena.slice(18);
+    alert(final);
+    var resultado
+}
+
