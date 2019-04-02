3deab6476b5ee40103ebdf09e3da0ce9c7b6f18d
nita86 <ana.rodriguezalv@gamil.com>
2019-04-03 00:07:16 +0200

    matrices terminado

--- MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_a_entregar/04-matrices/js/matrices.js
+++ MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_a_entregar/04-matrices/js/matrices.js
@@ -50,3 +50,22 @@ function ejercicio4() {
     var final = quitaComa1 + cadena.slice(18);
     document.getElementById('etiqueta4').innerHTML=final;
 }
+
+//ejercicio 5
+function ejercicio5() {
+    var entrada=document.getElementById("text5").value;//obtiene string
+    var array=entrada.split(",");//quita las comas y convierte en un array
+    var longitud=array.length;
+    //sort sobreescribe array y prevalece descendente, por lo que para mostrar el resultado hay que convertirlas en string
+    var ascendente=array.sort(function(a, b){return a-b});//ascendente
+    var cadenaAsc=ascendente.toString();
+    var descendente=array.sort(function(a, b){return b-a});//descendente
+    var cadenaDesc=descendente.toString();
+    //aleatorio
+    var aleatorio=new Array();//para que no sobreescriba sort se crea otro array sobre el que trabajamos
+    for(i=0;i<array.length;i++) {
+        var position =parseInt((Math.random() * 10));//genera numeros aleatorios en un rango 0-10 
+        aleatorio.push(array[position]);//los numeros generados con random position en array[position] se usan como los parametros que introduce push en aleatorio
+    }
+    document.getElementById('etiqueta5').innerHTML="matriz original: "+entrada+"<br> Ascendente: "+cadenaAsc+"<br> Descendente: "+cadenaDesc+"<br> Aleatorio: "+aleatorio+"<br> Longitud: "+longitud;
+}
