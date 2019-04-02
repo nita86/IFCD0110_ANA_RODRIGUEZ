2a17d9db725eab35034e622c0ddd25e343caf2b0
nita86 <ana.rodriguezalv@gmail.com>
2019-04-02 13:34:43 +0200

    02/04

--- MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_a_entregar/04-matrices/js/matrices.js
+++ MF0951_2_Integracion_componentes/UF1305_Programacion_web/Practicas_a_entregar/04-matrices/js/matrices.js
@@ -31,7 +31,22 @@ function ejercicio3() {
     var posicionComa=cadena.indexOf(",");
     var quitaComa1=cadena.slice(0,17);
     var final = quitaComa1 + cadena.slice(18);
-    alert(final);
-    var resultado
+    document.getElementById('etiquetiqueta3').innerHTML=final;
 }
 
+//ejercicio 4
+function ejercicio4() {
+    var entrada=document.getElementById("text4").value;//obtiene string
+    var comas=entrada.replace(/,/g, " ");//quitar comas y poner espacios en su lugar
+    var array=comas.split(" ");//quita espacios y convierte en un array
+    array.splice(0,0, "Grupo Volkswagen:");//introducir los valores al inicio
+    array.push("Volkswagen", "Bentley", "Bugatti");//introducir los valores al final
+    array.splice(1,0,"Ducati", "Lamborghini", "Porsche");//introducir los valores al en medio
+    var cadena=array.toString();
+    var seat=cadena.indexOf("seat");
+    alert(seat);
+    var posicionComa=cadena.indexOf(",");
+    var quitaComa1=cadena.slice(0,17);
+    var final = quitaComa1 + cadena.slice(18);
+    document.getElementById('etiquetiqueta3').innerHTML=final;
+}
---
+++ MF0951_2_Integracion_componentes/UF1306_Pruebas_web/Practicas_de_clase/js/funciones_bom.js
@@ -0,0 +1,51 @@
+//funciones JavaScript
+
+//tamaño ventana
+function tamanoVentana() {
+    var w = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    
+    var h = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+    
+    var x = document.getElementById("infoVentana");
+    x.innerHTML = "la anchura de la ventana del navegador: " + w + " y su altura es: " + h + ".";
+}
+
+//ancho y alto de la pantalla
+function pantalla() {
+    document.getElementById("divBom").innerHTML="el ancho de pantalla es "+screen.width+" y el alto es "+screen.height;
+}
+
+//ancho y alto de la pantalla disponible
+function pantallaDisponible() {
+    document.getElementById("divBom").innerHTML="el ancho de pantalla disponible es "+screen.availWidth+" y el alto disponible es "+screen.availHeight;
+}
+
+//profuncidad de color y pixeles
+function pantallaProfundidad() {
+    document.getElementById("divBom").innerHTML="la profundidad de color de la pantalla es "+screen.colorDepth+" y los pixeles "+screen.pixelDepth;
+}
+
+//información página web
+function urlPagina() {
+    document.getElementById("infoPantalla").innerHTML="la localización de la página es "+windows.location.href;
+}
+
+function dominiioPagina() {
+    document.getElementById("infoPantalla").innerHTML="el dominio de la página es "+windows.location.hostname;
+}
+
+function rutaPagina() {
+    document.getElementById("infoPantalla").innerHTML="la ruta de la página es "+window.location.pathname;
+}
+
+function protocoloPagina() {
+    document.getElementById("infoPantalla").innerHTML="el protocolo de la página es "+window.location.protocol;
+}
+
+function cargarPagina() {
+    document.getElementById("infoPantalla").innerHTML="la página asignada es "+window.location.assign("https://www.w3schools.com");
+}
