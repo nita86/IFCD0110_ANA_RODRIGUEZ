b0bd15c5e244839e4e0d46e0391a832faf42650e
nita86 <ana.rodriguezalv@gmail.com>
2019-04-10 13:54:31 +0200

    10/04

--- MF0951_2_Integracion_componentes/UF1306_Pruebas_web/Practicas_a_entregar/02_Imagen_LightBox/js/funciones_ejercicio02.js
+++ MF0951_2_Integracion_componentes/UF1306_Pruebas_web/Practicas_a_entregar/02_Imagen_LightBox/js/funciones_ejercicio02.js
@@ -6,11 +6,20 @@ function abrir() {
 function cerrar() {
     document.getElementById("cuadroGrande").style.display="none";
 }
+function cerrar2() {
+    document.getElementById("cuadroGrande").style.display="none";
+}
 //--------lightbox----------------
 function caja() {
     var fotos=document.getElementsByClassName("img");
-    for (i=0;i<fotos.length;i++) {
-        var insertar=document.getElementById("contenido").src=fotos[i];
-        fotos[i]=abrir()+insertar;
-    }
+    var txt = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+        txt = txt + x[i].src + "<br>";}
+}
+//-------------Paco
+function abrirFoto1() {
+    var foto='<img id="foto1" src="../img/house1.jpg" alt="House">';
+    document.getElementById("contenido").innerHTML=foto;
+    document.getElementById("cuadroGrande").style.display="block";
 }
