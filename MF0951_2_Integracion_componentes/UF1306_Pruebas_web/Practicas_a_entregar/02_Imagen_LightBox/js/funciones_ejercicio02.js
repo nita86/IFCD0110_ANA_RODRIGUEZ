d3b6dd73e6eddf35469c3c464dababe5e7781fe0
nita86 <ana.rodriguezalv@gamil.com>
2019-04-11 23:32:30 +0200

    11/04_casa

--- MF0951_2_Integracion_componentes/UF1306_Pruebas_web/Practicas_a_entregar/02_Imagen_LightBox/js/funciones_ejercicio02.js
+++ MF0951_2_Integracion_componentes/UF1306_Pruebas_web/Practicas_a_entregar/02_Imagen_LightBox/js/funciones_ejercicio02.js
@@ -9,8 +9,18 @@ function cerrar() {
 function cerrar2() {
     document.getElementById("cuadroGrande").style.display="none";
 }
-//--------lightbox----------------
+//--------lightbox Ana----------------
+function abrirFotoA(name) {//sacamos la información de this.id y la traemos a través del parametro name. Para que funcione, el id de la etiqueta« y el nombre del archivo deben coincidir (house1)
+    var foto='<img id="'+name+'cuadroGrande" src="../img/'+name+'.jpg" alt="'+name+ '">';
+    document.getElementById("contenido").innerHTML=foto;
+    document.getElementById("cuadroGrande").style.display="block";
+}
 
+function fotosSrc(src) {//sacamos la información de this.src y la traemos a través del parametro src. 
+    var foto='<img src="'+src+'">';
+    document.getElementById("contenido").innerHTML=foto;
+    document.getElementById("cuadroGrande").style.display="block";
+}
 //-------------Paco---------------
 function abrirFoto1() {
     var foto='<img id="foto1" src="../img/house1.jpg" alt="House">';
